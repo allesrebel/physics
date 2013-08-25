11c631d1526020bfa216a9375eeb5b3e218f54a9
Alles Rebel <allesrebel@yahoo.com>
2013-08-25 13:29:26 -0700

    addedd read me

---
+++ README.md
@@ -0,0 +1,7 @@
+#Vector Based Physics Engine#
+An experiment to see if I can implement a engine based on vectors. 
+Following http://gamedev.tutsplus.com/tutorials/implementation/create-custom-2d-physics-engine-aabb-circle-impulse-resolution/
+And basically making it my own. 
+
+##Dependencies##
+SDL 1.2 - for rendering 
--- src/physicsEngine/PhysicsEngine.h
+++ src/physicsEngine/PhysicsEngine.h
@@ -44,14 +44,16 @@ struct Circle{
 class PhysicsEngine {
 public:
 	PhysicsEngine();
+	virtual ~PhysicsEngine();
+
 	static bool checkCollisionAABB(AABB, AABB);
 	static bool checkCollisionCircle(Circle, Circle);
+
 	//math helper functions
 	static float getMagnitude(Vec2);
 	static Vec2 makeUnitVector(Vec2);
 	static Vec2 getNormal(Vec2, Vec2);
 	static float distance(Vec2, Vec2);
-	virtual ~PhysicsEngine();
 };
 
 #endif /* PHYSICSENGINE_H_ */
